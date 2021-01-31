952ba97a431750b1742ced8b363bee6ae2737ba4
Serhii <52746232+43raze@users.noreply.github.com>
2021-01-31 10:06:43 +0200

    Add files via upload
    new

---
+++ js/main.js
@@ -0,0 +1,96 @@
+'use strict';
+/* ---------------------------
+? Burger
+--------------------------- */
+$(function () {
+    $(".header__burger").click(function () {
+        $(".header__burger, .header__nav").toggleClass("active");
+        $("body").toggleClass("lock");
+    });
+});
+/* ---------------------------
+? Burger END
+--------------------------- */
+
+/* ---------------------------
+? Slick
+--------------------------- */
+$('.sl').slick({
+    slidesToShow: 4, // отображение количества слайдеров
+    slidesToScroll: 4,
+    //autoplay: true,
+    //autoplaySpeed: 2000, //время прокрутки 1000=1 сек
+    //speed: 1000, // скорость слайда
+    centerMode: false,
+    centerPadding: '0px',
+    dots: false,
+    arrows: true,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            arrows: true,
+            centerMode: true,
+            centerPadding: '0px',
+            slidesToShow: 1,
+            //slidesToScroll: 1,
+        }
+    }]
+
+    //fade: false, //смена слайдра способом затемнения
+    //draggable: false, //отмена принудительного переключения слайда
+    //infinite: false, // в конце нельзя принудительно прокрутить слайд
+    //vertical: true, // вертикальный слайдер
+});
+/* ---------------------------
+? Slick END
+--------------------------- */
+
+/* ---------------------------
+..? Top-Nav
+---------------------------
+var h_hght = 100; // высота шапки
+var h_mrg = 0;	  // отступ когда шапка уже не видна
+
+$(function () {
+
+    var elem = $('.header_nav_top');
+    var top = $(this).scrollTop();
+
+    if (top > h_hght) {
+        elem.css('top', h_mrg);
+    }
+
+    $(window).scroll(function () {
+        top = $(this).scrollTop();
+
+        if (top + h_mrg < h_hght) {
+            elem.css('top', (h_hght - top));
+        } else {
+            elem.css('top', h_mrg);
+        }
+    });
+
+});
+ ---------------------------
+..? Top-Nav END
+---------------------------
+
+ ---------------------------
+..? Tab
+---------------------------
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+ ---------------------------
+..? Tab END
+--------------------------- */
